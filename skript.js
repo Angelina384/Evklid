24bdf01eb1ddb977d516ab3d30072cbf8c95cf4c
let_me_panic <gela_sh@mail.ru>
2022-12-07 20:54:05 +0300

    ain

--- skript.js
+++ skript.js
@@ -16,17 +16,22 @@ const swiper = new Swiper('.swiper-container', {
     },
   });
 
-  document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('.tabs__btn').forEach(function(tabsBtn) {
-      tabsBtn.addEventListener('click', function(event) {
-        const path = event.currentTarget.dataset.path
-        
-  
-        document.querySelectorAll('.tab-content').forEach(function(tabContent) {
-          tabContent.classList.remove('tab-content-active')
+  document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('.tabs__btn').forEach(function (e) {
+        e.addEventListener('click', function (e) {
+            const tab = e.currentTarget.dataset.path;
+            document.querySelectorAll('.tab-content').forEach(function (e) {
+                e.classList.remove('tab-content-active')
+                document.querySelector(`[data-target='${tab}']`).classList.add('tab-content-active');
+            });
+        });
+
+        e.addEventListener('click', function (e) {
+            const tabDefault = e.currentTarget.dataset.default;
+            document.querySelectorAll('.tabs__btn').forEach(function (e) {
+                e.classList.remove('work__btn--default')
+                document.querySelector(`[data-path='${tabDefault}']`).classList.add('work__btn--default');
+            })
         })
-        document.querySelector(`[data-target="${path}"]`).classList.add('tab-content-active');
-      
-      })
-    })
-  })
+    });
+});
